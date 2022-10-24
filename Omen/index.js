a3d011e482d1ecb76f571c4960bac2f1da873a8f
hridayanshu <92839486+hridayanshu@users.noreply.github.com>
2022-10-24 16:29:22 +0545

    Add files via upload

---
+++ Omen/index.js
@@ -0,0 +1,27 @@
+function discord() {
+    location = "https://discordservers.com/panel/1022526775663800450/view"
+}
+
+function donate() {
+    location = "https://discordservers.com/panel/1022526775663800450/view"
+}
+
+function facebook() {
+    location = "https://www.facebook.com/hridayanshu.baniya.1/"
+}
+
+function insta() {
+    location = "https://www.instagram.com/hridayanshu_bani/"
+}
+
+function tiktok() {
+    location = "https://www.tiktok.com/@just_omen0"
+}
+
+function youtube() {
+    location = "https://www.youtube.com/channel/UCL22vYMvTyFab1NiIqPUoQQ"
+}
+
+function twitter() {
+    location = "https://twitter.com/JustOmen0"
+}
